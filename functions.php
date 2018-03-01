ee28e8f33ca1bfc830a5dbf4d7a92d005c1f153d
EvgputWeb <EvgputWeb@yandex.ru>
2018-03-01 15:14:25 +0300

    Доработки (рекурсивное решение задания 4 и др.)

--- functions.php
+++ functions.php
@@ -144,102 +144,7 @@ function task2($numArr, $operation)
 //##############################################################################
 // Задание #3
 
-define(
-    'ERR_MSG_WRONG_OPERATION',
-    "'<br>Первый параметр функции <b>task3</b> должен быть строкой,<br>\n" .
-    "содержащей один символ: '+', '-', '*' или '/'<br>\n"
-);
-
-
 function task3()
-{
-    $args = func_get_args();
-
-    // Проверка корректности входных параметров
-    if (count($args) < 2) {
-        echo "<br>Недостаточно входных параметров<br>\n";
-        return NULL;
-    }
-    //--------------------------------------------------------------------
-    // Тестируем первый входной параметр
-    $operation = $args[0];
-
-    // Инициализируем флаги для тестирования $operation
-    $operationTest1 = $operationTest2 = $operationTest3 = false;
-
-    // Тест 1: операция должна быть строкой
-    $operationTest1 = is_string($operation);
-
-    if ($operationTest1) { // Тест 1 прошли успешно
-        // Тест 2: длина строки должна быть 1 символ
-        $operation = trim($operation); // отбрасываем пробелы
-        $operationTest2 = (strlen($operation) == 1);
-
-        if ($operationTest2) { // Тест 2 прошли успешно
-            // Тест 3: операция должна быть + - * или /
-            $operationTest3 = (strpos('+-*/', $operation) !== false);
-        }
-    }
-    // Все три теста должны быть == true
-    // Если хотя бы один тест == false, то выходим из функции
-    if ((!$operationTest1) || (!$operationTest2) || (!$operationTest3)) {
-        echo ERR_MSG_WRONG_OPERATION;
-        return NULL; // или exception?
-    }
-    //--------------------------------------------------------------------
-    // Тестируем остальные входные параметры
-    define(
-        'ERR_MSG_WRONG_VALUES',
-        "<br>Некорректные входные параметры: ожидаются числовые значения<br>\n"
-    );
-
-    $goodValues = true;
-    for ($i = 1; $i < count($args); $i++) {
-        if (!is_numeric($args[$i])) {
-            $goodValues = false;
-            break;
-        }
-    }
-
-    if (!$goodValues) {
-        echo ERR_MSG_WRONG_VALUES;
-        return NULL; // или exception?
-    }
-
-    //--------------------------------------------------------------------
-    // Попали сюда - значит входные параметры корректные
-    $result = $args[1];
-
-    for ($i = 2; $i < count($args); $i++) {
-        switch ($operation) {
-            case '+':
-                $result += $args[$i];
-                break;
-            case '-':
-                $result -= $args[$i];
-                break;
-            case '*':
-                $result *= $args[$i];
-                break;
-            case '/':
-                // Ловим деление на ноль
-                $zeroDivider = false;
-                ($args[$i] == 0) ? ($zeroDivider = true) : ($result /= $args[$i]);
-                if ($zeroDivider) {
-                    echo ERR_MSG_ZERO_DIVISION;
-                    return NULL; // или exception?
-                }
-                break;
-        }
-    }
-
-    return $result;
-}
-
-//##############################################################################
-// Задание 3 с использованием вызова функции task2
-
-function task3_2()
 {
     $args = func_get_args();
 
@@ -261,84 +166,60 @@ function task3_2()
 //##############################################################################
 // Задание #4
 
+//---------------------------------------------------------------------------
+// Рекурсивная функция для вывода таблиц размером от (size x size) до (1 x 1)
+function print_mult_table($size) {
+
+    if ($size<1) {
+        // Выход из рекурсии
+        return;
+
+    } else {
+
+        echo "<table class=\"mult-table\">\n";
+        for ($i = 1; $i <= $size; $i++) {
+            echo "<tr>\n";
+            for ($j = 1; $j <= $size; $j++) {
+                echo "<td>" . $i * $j . "</td>";
+            }
+            echo "\n</tr>\n";
+        }
+        echo "</table>\n";
+        echo "<br><br>\n";
+
+        print_mult_table(--$size);
+
+    }
+}
+//---------------------------------------------------------------------------
+
 function task4($rowCount, $colCount)
 {
     if (!is_int($rowCount) || !is_int($colCount)) {
         echo "<br>Входные параметры должны быть целыми числами<br>\n";
         return NULL;
     }
+    if ($rowCount<>$colCount) {
+        echo "<br>Входные параметры должны быть одинаковыми<br>\n";
+        return NULL;
+    }
+
     //--------------------------------------------------------------------
     // Попали сюда - значит входные параметры корректные
 
     echo "<style>\n";
     echo ".mult-table { border: 2px solid black; border-collapse: collapse; text-align: center;}\n";
-    echo ".mult-table td { border: 1px solid black; width: 30px;}\n";
+    echo ".mult-table td { border: 1px solid black; width: 26px;}\n";
     echo "</style>\n";
 
-    echo "<table class=\"mult-table\">\n";
-    for ($i = 1; $i <= $rowCount; $i++) {
-        echo "<tr>\n";
-        for ($j = 1; $j <= $colCount; $j++) {
-            echo "<td>" . $i * $j . "</td>";
-        }
-        echo "\n</tr>\n";
-    }
-    echo "</table>\n";
+    print_mult_table($rowCount);
+
 }
+
 
 //##############################################################################
 //##############################################################################
 // Задание #5
-
-function task5($str)
-{
-    if (!is_string($str)) {
-        echo "<br>Входной параметр должен быть строкой<br>\n";
-        return NULL;
-    }
-
-    mb_internal_encoding("UTF-8");
-    if (mb_strlen($str) <= 1) {
-        echo "<br>В строке должно быть хотя бы 2 символа<br>\n";
-        return NULL;
-    }
-    //--------------------------------------------------------------------
-    // Попали сюда - значит входной параметр корректный
-    // Удаляем пробелы и переводим в нижний регистр
-    $str = mb_ereg_replace(' ', '', $str);
-    $str = mb_strtolower($str);
-
-    $reverseStr = '';
-    $len = mb_strlen($str);
-    for ($i = 0; $i < $len; $i++) {
-        $reverseStr .= mb_substr($str, $len - $i - 1, 1);
-    }
-
-    //echo "{" . $str . "}<br>\n";
-    //echo "{" . $reverseStr . "}<br>\n";
-
-    return ($reverseStr == $str);
-
-}
-
-//##############################################################################
-
-function show_task5_result($str)
-{
-    $res = task5($str);
-    if (!is_null($res)) {
-        echo "<b>" . $str . "</b><br>\n";
-        if ($res) {
-            echo "Данная строка является палиндромом<br><br>\n";
-        } else {
-            echo "Данная строка НЕ является палиндромом<br><br>\n";
-        }
-    }
-}
-
-//##############################################################################
-//##############################################################################
-// Рекурсивное решение задания #5
 
 //--------------------------------------------------------------------
 // Рекурсивная функция для тестирования совпадения символов
@@ -350,7 +231,7 @@ function test_equal_chars($str, $leftPos, $rightPos)
         // т.к. если не палиндром - вывалимся на раннем этапе рекурсии
         return true;
     }
-    if ($str[$leftPos] == $str[$rightPos]) {
+    if (mb_substr($str, $leftPos, 1) == mb_substr($str, $rightPos, 1)) {
         // Символы одинаковые - продолжаем
         return test_equal_chars($str, ++$leftPos, --$rightPos);
     } else {
@@ -362,16 +243,29 @@ function test_equal_chars($str, $leftPos, $rightPos)
 
 //--------------------------------------------------------------------
 
-function task5r($str)
+function task5($str)
 {
     if (!is_string($str)) {
         echo "<br>Входной параметр должен быть строкой<br>\n";
         return NULL;
     }
 
+    mb_internal_encoding("UTF-8");
+
+    if (mb_strlen($str) <= 1) {
+        echo "<br>В строке должно быть хотя бы 2 символа<br>\n";
+        return NULL;
+    }
+    //--------------------------------------------------------------------
+    // Попали сюда - значит входной параметр корректный
+
     echo "<br><b>$str</b>";
 
-    $res = test_equal_chars($str, 0, strlen($str) - 1);
+    // Удаляем пробелы и переводим в нижний регистр
+    $str = mb_ereg_replace(' ', '', $str);
+    $str = mb_strtolower($str);
+
+    $res = test_equal_chars($str, 0, mb_strlen($str) - 1);
 
     if ($res) {
         echo " - Палиндром<br>\n";
